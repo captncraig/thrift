192e0bf1ac3d55b026f97d792943cf24eede624b
Craig Peterson <cpeterson@ancestry.com>
2014-02-18 09:40:46 -0700

    Initial implementation of THttpSocket

---
+++ lib/csharp/src/Transport/THttpSocket.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+
+namespace Thrift.Transport
+{
+    public class THttpSocket : TServerTransport
+    {
+        private readonly HttpListener _listener;
+
+        public THttpSocket(int port)
+        {
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://*:"+port);
+        }
+
+        public override void Listen()
+        {
+            _listener.Start();
+        }
+
+        public override void Close()
+        {
+            _listener.Stop();
+        }
+
+        protected override TTransport AcceptImpl()
+        {
+            var context = _listener.GetContext();
+            return new TStreamTransport(context.Request.InputStream, context.Response.OutputStream);
+        }
+    }
+}
